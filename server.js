708b4b78cb34701f0c1bd1bc1ad32bc43337adcc
Ratnamani Tholeti <ratnamani.tholeti27@gmail.com>
2024-10-23 11:46:17 +0530

    second commit

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the Express server!"); // Send a message to the client
+  });
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/bookings', bookingRoutes);
